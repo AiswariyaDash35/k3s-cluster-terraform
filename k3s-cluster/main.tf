a6696281f6bc0d6ca619b2a38d06bde47c036f3c
AiswariyaDash35 <137080839+AiswariyaDash35@users.noreply.github.com>
2023-06-19 19:42:36 +0530

    Update main.tf

--- k3s-cluster/main.tf
+++ k3s-cluster/main.tf
@@ -1,1 +1,24 @@
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = "us-east-1"
+}
 
+module "master" {
+  source         = "./master"
+  ami            = var.ami
+  instance_type  = var.instance_type
+  key_name       = var.key_name
+  subnet_id      = var.subnet_id
+  security_group = var.security_group
+}
+
+module "worker" {
+  source            = "./worker"
+  ami               = var.ami
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  subnet_id         = var.subnet_id
+  security_group    = var.security_group
+  master_private_ip = module.master.master_private_ip
+}
